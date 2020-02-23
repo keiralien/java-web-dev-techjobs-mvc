5667eaf1e7f90c897be0896db5501e3346a6401c
keiralien <jen.murphy8200@gmail.com>
2020-02-23 16:38:37 -0600

    bonus 1 complete with default radio selection

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -22,6 +22,7 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("search", "");
         return "search";
     }
 
